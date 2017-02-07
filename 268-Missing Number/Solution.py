5290b724db2fa1f69ad4bf6045d5f525a5249b02
taoran <603133428@qq.com>
2017-02-07 22:04:27 +0800

    python solution

---
+++ 268-Missing Number/Solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        return sum(range(len(nums)+1))-sum(nums)
